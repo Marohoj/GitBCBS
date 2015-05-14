f70434a9129c8084e7a83d8be2ef6587885e7041
Mathias <marohoj@hotmail.com>
2015-05-14 13:06:32 +0200

    Merge

--- FinalCountdown/src/logic/UserMethod.java
+++ FinalCountdown/src/logic/UserMethod.java
@@ -10,6 +10,36 @@ public class UserMethod {
 	
 	private static Screen screen;
 
+	Connection myConn = null;
+	PreparedStatement myStmt = null;
+	ResultSet myRs = null;
+	
+	
+	IndstBc insertBC = new IndstBc();
+	private static Configurations cf = new Configurations();
+
+	
+	public static void insert() {
+
+			
+		try {
+			myConn = DriverManager.getConnection("jdbs:mysql:" + cf.getHost() + ":" + cf.getPort() + "/" + cf.getDBname());
+			
+			myStmt = 
+			String sql = "update balance "
+					+ "where first_name =" + get.IndstBC   ;
+		
+		myStmt.executeUpdate(sql);
+		
+		System.out.println("update complete");
+		
+		
+		}
+	}
+	
+	
+	
+	
 	public static void deposit() {
 		
 		screen = new Screen();
