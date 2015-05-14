77de2b305f720ebc2fab416c218ecb92f7cd5ca4
FUCKING SKODLORT <rasmus.s.dyhr@gmail.com>
2015-05-14 12:57:58 +0200

    Leg med SQLshit i UserMethod

--- FinalCountdown/src/logic/UserMetode.java
+++ FinalCountdown/src/logic/UserMetode.java
@@ -1,11 +1,39 @@
 package logic;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import com.mysql.jdbc.Statement;
+
+import db.Configurations;
+
 public class UserMetode {
 	
-	public static void test() {
+	Connection myConn = null;
+	PreparedStatement myStmt = null;
+	ResultSet myRs = null;
+	
+	
+	
+	IndstBc insertBC = new IndstBc();
+	private static Configurations cf = new Configurations();
+
+	
+	public static void insert() {
+
+			
+		try {
+			myConn = DriverManager.getConnection("jdbs:mysql:" + cf.getHost() + ":" + cf.getPort() + "/" + cf.getDBname());
+			
+			myStmt = 
+			String sql = "update balance "
+					+ "where first_name =" + get.IndstBC   ;
 		
-		System.out.println("Hej med dig");
+		myStmt.executeUpdate(sql);
 		
+		System.out.println("update complete");
+		
+		
+		}
 	}
 	
 
