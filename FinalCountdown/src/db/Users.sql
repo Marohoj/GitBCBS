eca72470a4df690720ad13ccc9cacbded77feba4
Mathias <marohoj@hotmail.com>
2015-05-19 13:33:09 +0200

    Database ændring

--- FinalCountdown/src/db/Users.sql
+++ FinalCountdown/src/db/Users.sql
@@ -6,23 +6,23 @@ CREATE TABLE `users`(
 `id` int(11) NOT NULL AUTO_INCREMENT, 
 `first_name` varchar(20) NOT NULL,
 `last_name` varchar(50) NOT NULL,
-`initials` varchar(8) NOT NULL,
-`password` varchar(8) NOT NULL,
+`initials` varchar(50) NOT NULL,
+`password` varchar(20) NOT NULL,
 `balance` double NOT NULL,
 PRIMARY KEY (`id`)) AUTO_INCREMENT = 4;
 
-INSERT INTO `users` VALUES ('1','Mathias', 'Højgaard', 'maho14am', '1234', 1.0), 
-('2','Rasmus', 'Dyhr', 'rady13ac', '2345', 1.0), 
-('3','Kristian', 'Koch', 'kran14ad', '3456', 1.0),
-('4', 'Christine', 'Pelzer', 'homo', '4567', 1.0);
+INSERT INTO `users` VALUES ('1','Mathias', 'Højgaard', 'maho14am@student.cbs.dk', '1234aaaa', 1.0), 
+('2','Rasmus', 'Dyhr', 'rady13ac@student.cbs.dk', '1234bbbb', 1.0), 
+('3','Kristian', 'Koch', 'kran14ad@student.cbs.dk', '1234cccc', 1.0),
+('4', 'Christine', 'Pelzer', 'chpe14ar@student.cbs.dk', '1234dddd', 1.0);
 
 DROP TABLE IF EXISTS admin;
 CREATE TABLE admin(
 id int(11) NOT NULL AUTO_INCREMENT, 
-name varchar(10),
-initials varchar(6),
-password varchar(8),
+name varchar(20),
+initials varchar(20),
+password varchar(20),
 currency double,
 PRIMARY KEY (id)) AUTO_INCREMENT = 1;
 
-INSERT INTO admin VALUES ('1', 'CBS_ADMIN', 'Adm', '1234', 150);
+INSERT INTO admin VALUES ('1', 'CBS_ADMIN', '88888888', '1234abcd', 150);
